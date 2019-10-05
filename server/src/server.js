5f953c60a47b911469400d514ef47b5432daa562
felipealvescosta <felipealves00007@gmail.com>
2019-10-05 14:30:33 -0300

    Projeto AirCnC da semana omnistack 9

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const routes = require('./routers');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://felipealves:Res3456res@cluster0-ubw8y.mongodb.net/aircnc?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://felipealves:suasenha@cluster0-ubw8y.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser:true,
     useUnifiedTopology:true,
 });
